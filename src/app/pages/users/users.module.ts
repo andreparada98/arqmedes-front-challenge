3ce55d7fcf9f4965bdfe69768a6db79ca8731e9a
Andre <andreparada98@gmail.com>
2023-09-14 14:28:25 -0300

    chore: import in users.module

--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersListComponent } from './users-list/users-list.component';
 import { MaterialModule } from 'src/app/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UsersListComponent],
-  imports: [CommonModule, UsersRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    UsersRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
 })
 export class UsersModule {}
