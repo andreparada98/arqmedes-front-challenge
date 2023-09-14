ac4af4c40884e235ce8680cd4a4ec2c54fe81094
Andre <andreparada98@gmail.com>
2023-09-14 12:44:11 -0300

    feat: add table in users-list

--- src/app/pages/users/users-list/users-list.component.ts
+++ src/app/pages/users/users-list/users-list.component.ts
@@ -1,7 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { UsersListService } from './users-list.service';
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { BaseComponent } from 'src/shared/model/base-component';
+import { UsersListResponse } from './models/users-list.response';
+import { MatTableDataSource } from '@angular/material/table';
+import { PageEvent } from '@angular/material/paginator';
+import { UsersListRequestDto } from './models/users-list.dto';
+import { BaseListResponseMeta } from 'src/shared/model/base-list.model';
 
 @Component({
   selector: 'app-users-list',
@@ -9,15 +13,46 @@ import { BaseComponent } from 'src/shared/model/base-component';
   styleUrls: ['./users-list.component.scss'],
 })
 export class UsersListComponent extends BaseComponent implements OnInit {
-  constructor(private httpClient: HttpClient) {
+  users!: MatTableDataSource<UsersListResponse>;
+  displayedColumns: string[] = [
+    'nome',
+    'dataNascimento',
+    'cpf',
+    'cidade',
+    'actions',
+  ];
+
+  meta: BaseListResponseMeta = {
+    page: 1,
+    itemsPerPage: 10,
+    totalItems: 0,
+  };
+
+  constructor(private usersListService: UsersListService) {
     super();
   }
-  ngOnInit(): void {}
-
-  teste(): void {
-    this.httpClient
-      .get(`${environment.api_url}/users`)
-      .pipe()
-      .subscribe((res) => console.log(res));
+  ngOnInit(): void {
+    this.getUsersOrThrow({});
   }
+
+  private getUsersOrThrow(payload: UsersListRequestDto) {
+    this.usersListService.getUsers(payload).subscribe((res) => {
+      this.users = new MatTableDataSource(res.data);
+      this.meta = res.meta;
+    });
+  }
+
+  public onPageChange(event: PageEvent) {
+    const request: UsersListRequestDto = {
+      page: event.pageIndex + 1,
+      pageSize: event.pageSize,
+    };
+    this.getUsersOrThrow(request);
+  }
+
+  viewUser(user: UsersListResponse) {}
+
+  editUser(user: UsersListResponse) {}
+
+  deleteUser(user: UsersListResponse) {}
 }
